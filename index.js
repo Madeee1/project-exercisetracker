a1578c05a1d0500834f60d7e74bbf0aa6b01d4b8
Madeee1 <madeee@duck.com>
2023-05-18 09:44:16 +0000

    feat: function 1 post /api/users with username

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 });
 
 // 1. You can POST to /api/users with form data username to create a new user.
+app.post(
+  "/api/users",
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    const username = req.body.username;
 
+    //Call the mongoose function to get username from the database
+    try {
+      const user = await getIdFromUser(username);
+      res.json({ username: username, _id: user._id });
+    } catch (err) {
+      console.error("Error in POST /api/users: ", err);
+    }
+  }
+);
 // 2. The returned response from POST /api/users with form data username will be an object with username and _id properties.
 
 // The use of MongoDB and mongoose below here
@@ -32,7 +46,7 @@ mongoose
 
 // Schema
 const userExerciseSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   // Array
   log: [
     {
@@ -57,6 +71,19 @@ const UserExercise = mongoose.model(
 
 //Function to get the id by username parameter
 const getIdFromUser = async (username) => {
-  const user = await UserExercise.findOne({ username: username });
-  return user._id;
+  // Try to find the id, if not found, create a new user
+  try {
+    const user = await UserExercise.findOne({ username: username });
+    return user._id;
+  } catch (err) {
+    const newUser = new UserExercise({ username: username });
+
+    try {
+      await newUser.save();
+      return newUser._id;
+    } catch (err) {
+      console.error("Error in getIdFromUser: ", err);
+      return Promise.reject(err);
+    }
+  }
 };
